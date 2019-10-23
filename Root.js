02f9c397c71993b23539c64a02bc3e2a203fcf3b
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-10-22 22:46:59 -0500

    added more screens to the nav stack

--- Root.js
+++ Root.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 //import screens here
-import HomeScreen from './src/screens/Home';
-// import GameScreen from './src/screens/Game';
+import HomeScreen from './src/screens/Home.js';
+import GameSetupScreen from './src/screens/GameSetup.js';
+import ScoreboardScreen from './src/screens/Scoreboard.js';
+import ReadyUpScreen from './src/screens/ReadyUp.js';
+import FinalResultsScreen from './src/screens/FinalResults.js';
 
 const RootStack = createStackNavigator(
     {
         Home: HomeScreen,
-        // Game: GameScreen
+        GameSetup: GameSetupScreen,
+        Scoreboard: ScoreboardScreen,
+        ReadyUp: ReadyUpScreen,
+        FinalResults: FinalResultsScreen
     },
     {
         initialRouteName: "Home"
