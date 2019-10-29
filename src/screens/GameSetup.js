6270b9f04014ae9b018ebe33d310ad688180c71e
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-10-28 23:46:57 -0500

    added to gamesetup screen

--- src/screens/GameSetup.js
+++ src/screens/GameSetup.js
@@ -1,19 +1,104 @@
 //GameSetup
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
 
-class GameSetup extends React.Component {
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+} from 'react-native';
+import OutputT from '../components/GameSetup/OutputT.js'
+
+class GameSetup extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Game Setup Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+      <View style={styles.container}>
+        <View style={styles.headingBox}>
+          <Text style={styles.heading1}>
+            PLAYERS
+          </Text>
+        </View>
+        <View style={styles.playerListBox}>
+          <Text style={styles.heading2}>
+            Enter names below:
+          </Text>
+          <OutputT />
+        </View>
+        <View style={styles.roundSelectBox}>
+          <Text style={styles.heading2}>
+            Players will play each other:
+          </Text>
+          <Text style={styles.heading3}>
+            Round Select Goes Here
+          </Text>
+        </View>
+        <View style={styles.buttonBox}>
+          <Button 
+            title="Play!"
+            color="purple"
+            onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
       </View>
     );
   }
 }
- 
+
 export default GameSetup;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    backgroundColor: 'mintcream',
+  },
+  headingBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    margin: 20,
+    marginBottom: 5,
+	},
+	playerListBox: {
+    flex: 5,
+    backgroundColor: '#ffffff',
+		marginHorizontal: 20,
+  },
+  roundSelectBox: {
+    flex: 2,
+    backgroundColor: '#abc5cf',
+    margin: 20,
+    marginVertical: 0,
+  },
+  buttonBox: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    margin: 20,
+  },
+  heading1: {
+    fontSize: 48,
+    color: '#000000',
+    textAlign: 'center',
+    margin: 5,
+	},
+	heading2: {
+    fontSize: 28,
+    color: '#7e0fa6',
+    textAlign: 'center',
+    margin: 10,
+  },
+  heading3: {
+    fontSize: 24,
+    color: '#3d3d3d',
+    textAlign: 'center',
+    margin: 5,
+  },
+});
