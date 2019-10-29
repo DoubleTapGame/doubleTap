c5ab2cc63d7588c93da798126713041bbbaef582
Nida Hameed <nidahameed@Nidas-MacBook-Pro.local>
2019-10-28 23:33:33 -0500

    made home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,57 @@
-//HomeScreen goes here
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback,
+} from 'react-native';
 
-class Home extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+      <View style={styles.ViewContainer}>
+        <Text style={styles.TextContainer}>Double Tap</Text>
+
+          <View style={styles.ButtonBox}>
+            <TouchableHighlight>
+              <View style={styles.CustomButton}>
+                <Text>Play!</Text>
+              </View>
+            </TouchableHighlight>
+        </View>
+          
       </View>
     );
   }
 }
- 
-export default Home;
+
+const styles = StyleSheet.create(
+  {
+    ViewContainer:
+    {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#6816D3',
+    },
+    TextContainer:
+    {
+      color: 'white',
+      marginBottom: 200,
+      //fontFamily: Bangers,
+    },
+    ButtonBox:
+    {
+      backgroundColor: 'white',
+      justifyContent: 'space-between',
+      paddingTop: 10,
+      paddingBottom: 10,
+      paddingRight: 80,
+      paddingLeft: 80,
+    }
+    
+   
+
+  });
