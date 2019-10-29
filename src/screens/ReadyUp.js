4afe3940bbd0ab1fcfdf28f61bede6b382f7659c
Mikey Cabrera <mikeycabrera@Mikeys-MacBook-Pro.local>
2019-10-29 00:39:12 -0500

    started styling the get ready screen

--- src/screens/ReadyUp.js
+++ src/screens/ReadyUp.js
@@ -1,15 +1,30 @@
-//ReadyUp
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class ReadyUp extends React.Component {
     render() {
       return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Ready Up Screen</Text>
+        <>
+        <View>
+          <Text style={{justifyContent: 'center'}}>Ready Up Screen</Text>
         </View>
+        <View style ={{flex: 1}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+            <Text style = {styles.textRotate}>Player 1</Text>
+          </View>
+          <View style={{flex: 1, backgroundColor: 'skyblue'}} >
+            <Text>Player 2</Text>
+          </View>
+        </View>
+        </>
       );
     }
 }
-   
+
+const styles = StyleSheet.create ({
+    textRotate:{
+    transform: [{ rotate: '180deg'}],
+    alignItems: 'center'
+    }
+})
 export default ReadyUp;
